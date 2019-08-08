30893905f268a359bb1d0ba284550536709ca0ae
ngaruko <ngarukob@yahoo.com>
2019-08-08 22:13:02 +1200

    more bindings

--- graphql-prisma/prisma.js
+++ graphql-prisma/prisma.js
@@ -9,9 +9,21 @@ const prisma = new Prisma({
 
 });
 
-prisma.query.users(null, '{id name email posts {id title}}').then((data) => {
-	//console.log(JSON.stringify(data,undefined,2));
-});
+//async
+const createPostForUser = async (authorId,data) => {
+	
+};
+
+
+// prisma.exists.User({
+// name:"Bede Ngaruko "
+// }).then((result) => log(result));
+// prisma.exists.Post({
+// 	id: "cjz2c34q300ev0807zscgzk5n"
+// }).then((result) => log(result));
+// prisma.query.users(null, '{id name email posts {id title}}').then((data) => {
+// 	console.log(JSON.stringify(data,undefined,2));
+// });
 
 // prisma.mutation.createUser({
 // 	data: {
@@ -23,26 +35,27 @@ prisma.query.users(null, '{id name email posts {id title}}').then((data) => {
 // 		log(data);
 // 	});
 
-prisma.mutation.createPost({
-	data: {
-		title: "Chaining promises",
-		body: "The story of Chinua achebe about brits in nigeria",
-		published: true,
-		author: {
-			connect: {
-				email: "bede@ngaruko.com"
-			}
-		}
+// prisma.mutation.createPost({
+// 	data: {
+// 		title: "Chaining promises",
+// 		body: "The story of Chinua achebe about brits in nigeria",
+// 		published: true,
+// 		author: {
+// 			connect: {
+// 				email: "bede@ngaruko.com"
+// 			}
+// 		}
 			
-	}
-},
-	'{id title body published author{name} }').then((data) => {
+// 	}
+// },
+// 	'{id title body published author{name} }').then((data) => {
 
-		log(JSON.stringify(data, undefined, 2));
-		return prisma.query.users(null, '{id name email posts {id title}}');
+// 		log(JSON.stringify(data, undefined, 2));
+// 		return prisma.query.users(null, '{id name email posts {id title}}');
 		
-	}).then((data) => {
+// 	}).then((data) => {
 
-		log(JSON.stringify(data, undefined, 2));
+// 		log(JSON.stringify(data, undefined, 2));
 
-	});
+// 	});
+
