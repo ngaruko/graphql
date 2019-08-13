364b05734e7a880449d8ce4dd1e472a43a579931
ngaruko <ngarukob@yahoo.com>
2019-08-13 15:36:19 +1200

    mutations and exists method

--- graphql-prisma/prisma.js
+++ graphql-prisma/prisma.js
@@ -1,19 +1,68 @@
-import { Prisma } from 'prisma-binding';
-import { log } from 'util';
+import { Prisma } from "prisma-binding";
+import { log } from "util";
+import chalk from 'chalk';
 
 
 const prisma = new Prisma({
-			"schemaPath": 'src/generated/prisma.graphql',
-	typeDefs: 'src/generated/prisma.graphql',
-	endpoint: 'http://localhost:4466'
-
+  schemaPath: "src/generated/prisma.graphql",
+  typeDefs: "src/generated/prisma.graphql",
+  endpoint: "http://localhost:4466"
 });
 
-//async
-const createPostForUser = async (authorId,data) => {
-	
+// //async
+// const createPostForUser = async (authorId, data) => {
+// 	//check if user exists
+// 	const userExists = await prisma.exists.User({ id: authorId });
+// 	if (!userExists) {
+// 		throw new Error(`No user was found with Id ${authorId}...`);
+// 	}
+//   const post = await prisma.mutation.createPost(
+//     {
+//       data: {
+//         ...data,
+//         author: {
+//           connect: {
+//             id: authorId
+//           }
+//         }
+//       }
+//     },
+//     `{author {id name email posts{id title published}}}`
+//   );
+
+//   return post.author;
+// };
+
+// // use the function
+// createPostForUser("cjz22gdq7000g08070s1i9zwu", {
+//   title: "The new World falls appart",
+//   body:
+//     " A remarkable story of British occupation of Nigeria, by Chinua Achebe",
+//   published: true
+// }).then((user) => log(JSON.stringify(user, undefined, 2))).catch((error) => log(chalk.red(error.message)));
+
+
+
+//update post for user
+const updatePostForUser = async (postId, data) => {
+  const postExists = await prisma.exists.Post({ id: postId });
+  if (!postExists) {
+    throw new Error(`No post was found with Id ${postId}...`);
+  }
+
+  //update
+  log(chalk.green(`Updating post with id ${postId}....`));
+  const post = await prisma.mutation.updatePost({
+    where: {
+      id:postId
+    },
+    data
+  }, `{author {id name email posts{id title published}}}`);
+  return post.author;
 };
 
+updatePostForUser('cjz237u6j0009070777yw22y2', {published: false}).then((user) => log(chalk.green(JSON.stringify(user, undefined, 2))))
+  .catch((error) => log(chalk.red(error.message)));
 
 // prisma.exists.User({
 // name:"Bede Ngaruko "
@@ -45,17 +94,16 @@ const createPostForUser = async (authorId,data) => {
 // 				email: "bede@ngaruko.com"
 // 			}
 // 		}
-			
+
 // 	}
 // },
 // 	'{id title body published author{name} }').then((data) => {
 
 // 		log(JSON.stringify(data, undefined, 2));
 // 		return prisma.query.users(null, '{id name email posts {id title}}');
-		
+
 // 	}).then((data) => {
 
 // 		log(JSON.stringify(data, undefined, 2));
 
 // 	});
-
